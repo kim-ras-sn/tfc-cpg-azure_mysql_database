41d9f1c711a098adef084425f4dedf7088ea8774
Kim <36648257+kim-ras@users.noreply.github.com>
2021-04-25 11:43:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ provider "azurerm" {
   tenant_id       = var.tenantId
 }
 
-resource "azurerm_resource_group" "cpg-tfc-mysqldb-rg" {
-  name     = "cpg-tfc-mysqldb-rg"
+resource "azurerm_resource_group" "cpg-tfc-mysqlsrv-rg" {
+  name     = "cpg-tfc-mysqlsrv-rg"
   location = "${var.location}"
   tags = {
     CostCenter = "${var.costcenter}"
@@ -17,15 +17,15 @@ resource "azurerm_resource_group" "cpg-tfc-mysqldb-rg" {
 }
  
 resource "azurerm_mysql_server" "mysqldb-instance1" {
-  name                = "${var.db_name}"
-  location            = azurerm_resource_group.cpg-tfc-mysqldb-rg.location
-  resource_group_name = azurerm_resource_group.cpg-tfc-mysqldb-rg.name
-  tags                = azurerm_resource_group.cpg-tfc-mysqldb-rg.tags
+  name                = "${var.dbsrv_name}"
+  location            = azurerm_resource_group.cpg-tfc-mysqlsrv-rg.location
+  resource_group_name = azurerm_resource_group.cpg-tfc-mysqlsrv-rg.name
+  tags                = azurerm_resource_group.cpg-tfc-mysqlsrv-rg.tags
   
   administrator_login          = "${var.bbdd_admin_user}"
   administrator_login_password = "${var.bbdd_admin_pwd}"
  
-  sku_name   = "B_Gen5_2"
+  sku_name   = "B_Gen5_1"
   storage_mb = 5120
   version    = "5.7"
  
