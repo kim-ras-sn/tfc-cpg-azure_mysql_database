a2775d6181b74937c69896739b47cd0f1b84c45f
Kim <36648257+kim-ras@users.noreply.github.com>
2021-04-29 15:29:39 +0200

    added fqdn to output

--- output.tf
+++ output.tf
@@ -2,15 +2,7 @@ output "mqsql_name" {
   value       = azurerm_mysql_server.mysqldb-instance1.name
   description = "The name of the MySql db server"
 }
-#output "mqsql_public_ip_addr" {
-#  value       = azurerm_mysql_server.mysqldb-instance1.public_ip
-#  description = "The public IP address of the MySql db server"
-#}
-#output "mqsql_public_dns" {
-#  value       = azurerm_mysql_server.mysqldb-instance1.fully_qualified_domain_name
-#  description = "The public DNS address of the MySql db server"
-#}
-#output "mqsql_priv_ip_addr" {
-#  value       = azurerm_mysql_server.mysqldb-instance1.private_ip
-#  description = "The private IP address of the MySql db server"
-#}
+output "mqsql_public_dns" {
+  value       = azurerm_mysql_server.mysqldb-instance1.fqdn
+  description = "The public DNS address of the MySql db server"
+}
