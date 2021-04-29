442f3adc08b26f68f9c6768ee6ed09ba44f3c8fc
Kim <36648257+kim-ras@users.noreply.github.com>
2021-04-29 15:30:09 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "mqsql_name" {
   value       = azurerm_mysql_server.mysqldb-instance1.name
   description = "The name of the MySql db server"
 }
-output "mqsql_public_dns" {
+output "mqsql_fqdn" {
   value       = azurerm_mysql_server.mysqldb-instance1.fqdn
   description = "The public DNS address of the MySql db server"
 }
