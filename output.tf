ffa48756810e58f640311b01eecd37108f9ea1a1
Kim <36648257+kim-ras@users.noreply.github.com>
2021-04-29 15:08:23 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,15 +2,15 @@ output "mqsql_name" {
   value       = azurerm_mysql_server.mysqldb-instance1.name
   description = "The name of the MySql db server"
 }
-output "mqsql_public_ip_addr" {
-  value       = azurerm_mysql_server.mysqldb-instance1.public_ip
-  description = "The public IP address of the MySql db server"
-}
+#output "mqsql_public_ip_addr" {
+#  value       = azurerm_mysql_server.mysqldb-instance1.public_ip
+#  description = "The public IP address of the MySql db server"
+#}
 output "mqsql_public_dns" {
-  value       = azurerm_mysql_server.mysqldb-instance1.public_dns
-  description = "The public IP address of the MySql db server"
+  value       = azurerm_mysql_server.mysqldb-instance1.fully_qualified_domain_name
+  description = "The public DNS address of the MySql db server"
 }
-output "mqsql_priv_ip_addr" {
-  value       = azurerm_mysql_server.mysqldb-instance1.private_ip
-  description = "The private IP address of the MySql db server"
-}
+#output "mqsql_priv_ip_addr" {
+#  value       = azurerm_mysql_server.mysqldb-instance1.private_ip
+#  description = "The private IP address of the MySql db server"
+#}
